9893a87410c2df0ced6bb4cbcd957219ed84627a
Yvonne <Yvonne@VVOLF>
2018-01-05 12:37:15 +1100

    Successfully added a way to pass variables over to different forms. Comitting code. Still need to get it working for the other two variables and then try to create the connection using those.

--- TestAppWithConnection/Main.Designer.cs
+++ TestAppWithConnection/Main.Designer.cs
@@ -29,26 +29,39 @@
         private void InitializeComponent()
         {
             this.EndSession = new System.Windows.Forms.Button();
+            this.OpenConn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // EndSession
             // 
-            this.EndSession.Location = new System.Drawing.Point(55, 48);
+            this.EndSession.Location = new System.Drawing.Point(544, 198);
             this.EndSession.Name = "EndSession";
-            this.EndSession.Size = new System.Drawing.Size(596, 170);
+            this.EndSession.Size = new System.Drawing.Size(176, 51);
             this.EndSession.TabIndex = 0;
-            this.EndSession.Text = "Close the Current Session";
+            this.EndSession.Text = "Exit Program";
             this.EndSession.UseVisualStyleBackColor = true;
             this.EndSession.Click += new System.EventHandler(this.EndSession_Click);
+            // 
+            // OpenConn
+            // 
+            this.OpenConn.Location = new System.Drawing.Point(181, 48);
+            this.OpenConn.Name = "OpenConn";
+            this.OpenConn.Size = new System.Drawing.Size(138, 23);
+            this.OpenConn.TabIndex = 1;
+            this.OpenConn.Text = "Open A Connection";
+            this.OpenConn.UseVisualStyleBackColor = true;
+            this.OpenConn.Click += new System.EventHandler(this.OpenConn_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(732, 261);
+            this.Controls.Add(this.OpenConn);
             this.Controls.Add(this.EndSession);
             this.Name = "Main";
             this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load_1);
             this.ResumeLayout(false);
 
         }
@@ -56,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button EndSession;
+        private System.Windows.Forms.Button OpenConn;
     }
 }
